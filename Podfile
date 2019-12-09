6dadd647c27c19f30f0550a0d9a574908b3caabd
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2019-12-10 01:31:13 +0530

    Splash screen ui complete

---
+++ Podfile
@@ -0,0 +1,9 @@
+ platform :ios, '10.0'
+
+target 'The Pantry' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for The Pantry
+    pod 'DotsLoading'
+end
--- The Pantry/Controller/ViewController.swift
+++ The Pantry/Controller/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import DotsLoading
 
 class ViewController: UIViewController {
 
@@ -14,9 +15,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("yep")
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        self.startDotAnimation()
+    }
+}
 
-    
+extension ViewController{
+    private func startDotAnimation(){
+        //Creating loading animation
+        let dotColors = [UIColor.black, UIColor.black, UIColor.black, UIColor.white]
+        let loadingView = DotsLoadingView(colors: dotColors)
+        self.view.addSubview(loadingView)
+        //Adding contrains
+        loadingView.translatesAutoresizingMaskIntoConstraints = false
+        loadingView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -60).isActive = true
+        loadingView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        //starting animation
+        loadingView.show()
+    }
 }
