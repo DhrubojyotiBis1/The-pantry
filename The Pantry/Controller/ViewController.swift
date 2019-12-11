1e1df829b7480ead45cc836c44e706e0a4992b34
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2019-12-12 04:21:26 +0530

    Splash Screen logic integreated except Download image and Error handeling

--- The Pantry/Controller/ViewController.swift
+++ The Pantry/Controller/ViewController.swift
@@ -10,36 +10,75 @@ import UIKit
 import DotsLoading
 
 class ViewController: UIViewController {
+    
+    var destinationSegueId = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.decideDestinationSegueID()
+        self.neworking()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        navigationController?.navigationBar.isHidden = true
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(true)
-        self.startDotAnimation()
-        performSegue(withIdentifier: segueId.registrationVCId, sender: nil)
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == segueId.HomeVCId){
+            //send the images to the Home VC
+        }
     }
 }
 
+//MARK:- Networking stuff
 extension ViewController{
-    private func startDotAnimation(){
-        //Creating loading animation
+    private func neworking() {
+        let animator = createAnimatorDotView()
+        self.addConstrain(toAnimator: animator)
+        animator.show()
+        
+        Networking().downloadImageForHomeViewControllerBanner(havingUrls: nil) { (result) in
+            if(result){
+                //Pass the image array to the home VC
+                print("Image download complete")
+            }else{
+                //handel the error
+                print("Image download fails")
+            }
+            animator.stop()
+            //going to home VC
+            self.performSegue(withIdentifier: self.destinationSegueId, sender: nil)
+        }
+    }
+}
+
+//All private function expet netwoking stuff
+extension ViewController{
+    
+    //decides which VC to go
+    private func decideDestinationSegueID(){
+        let savedCredentials = save().getCredentials()
+        if(savedCredentials[credential.token] != nil){
+            //go to home VC
+            self.destinationSegueId = segueId.HomeVCId
+        }else{
+            // go to Register VC
+            self.destinationSegueId = segueId.registrationVCId
+        }
+    }
+    
+    //Creating loading animation
+    private func createAnimatorDotView()->DotsLoadingView{
         let dotColors = [UIColor.black, UIColor.black, UIColor.black, UIColor.white]
         let loadingView = DotsLoadingView(colors: dotColors)
-        self.view.addSubview(loadingView)
+        return loadingView
+    }
+    
+    
+    private func addConstrain(toAnimator animator:DotsLoadingView){
+        //adding to the viewController View
+        self.view.addSubview(animator)
         //Adding contrains
-        loadingView.translatesAutoresizingMaskIntoConstraints = false
-        loadingView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -60).isActive = true
-        loadingView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
-        //starting animation
-       // loadingView.show()
+        animator.translatesAutoresizingMaskIntoConstraints = false
+        animator.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -60).isActive = true
+        animator.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
     }
 }
