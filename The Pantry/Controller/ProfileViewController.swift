78f58e047a8f951c87cea71534bd8034012dce8f
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2019-12-11 13:41:47 +0530

    Profile , Edit Profile and Change Password UI complete

---
+++ The Pantry/Controller/ProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewController.swift
+//  The Pantry
+//
+//  Created by Dhrubojyoti on 10/12/19.
+//  Copyright © 2019 coded. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    let cornerRadius:CGFloat = 10.0
+    @IBOutlet var seperateButtons: [UIButton]!
+    @IBOutlet var seperateViews: [UIView]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func changePasswordButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func logoutButtonPressed(_ sender: Any) {
+    }
+    
+}
