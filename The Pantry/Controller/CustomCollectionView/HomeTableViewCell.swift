882a9b5e7542a889ab46e36867fe2659a6f43f19
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2020-01-30 01:21:28 +0530

    Home Page UI reconstruction complete

---
+++ The Pantry/Controller/CustomCollectionView/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  The Pantry
+//
+//  Created by Dhrubojyoti on 29/01/20.
+//  Copyright © 2020 coded. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
